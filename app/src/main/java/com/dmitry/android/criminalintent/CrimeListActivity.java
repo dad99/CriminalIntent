567ee0a202eea668a20efd8e25c2100cf7aa6085
dad99 <dad948@gmail.com>
2018-04-05 20:40:20 +0300

    Home 05.04

---
+++ app/src/main/java/com/dmitry/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,12 @@
+package com.dmitry.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
+// 195 Something wrong!!!
