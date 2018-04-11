648768d3269be7041ea4459934fbb9ba2d1ecc90
dad99 <dad948@gmail.com>
2018-04-11 15:24:01 +0300

    11.04 From work Rewrote Crime, CrimeActivity, CrimeFragment and layouts Deleted other files, they'll be rewritten

--- app/src/main/java/com/dmitry/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/dmitry/android/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.dmitry.android.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,14 @@ public class CrimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
         }
     }
